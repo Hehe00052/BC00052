2a9db88ec346051e6b4ef65453c3fdf762401c16
Phuong <phuonghtnbc00052@fpt.edu.vn>
2023-07-06 14:27:17 +0700

    done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,5 +89,7 @@
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
+<footer>
+        &copy; BC00052-ATN Shop
+    </footer>
 </html>
